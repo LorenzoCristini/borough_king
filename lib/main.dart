a8c1e17288d736d359513938e0bfb01144958edd
ChiaraGiaca <gia_chiara@yahoo.it>
2021-01-11 20:01:12 +0100

    fatto

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Borough King",
+        title: "KEK",
         home: HomePage(),
     );
   }
