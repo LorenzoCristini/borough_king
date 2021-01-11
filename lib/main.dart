79ad5bf63dd87ce3d7d24fde4cc7841abef7e2e6
Nakeddo <lorenzo-cristini@hotmail.it>
2021-01-11 20:41:12 +0100

    epico

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "pazzesco King",
+      title: "Borough King",
       home: HomePage(),
     );
   }
