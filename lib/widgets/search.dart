b4f44597f548fa8c102dd477565eb43160e3964c
Nakeddo <lorenzo-cristini@hotmail.it>
2021-01-15 16:49:04 +0100

    search

--- lib/widgets/search.dart
+++ lib/widgets/search.dart
@@ -1,9 +1,10 @@
 import 'dart:math';
 import 'package:borough_king/main.dart';
+import 'package:borough_king/widgets/preferiti.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import "package:flutter/material.dart";
-import "package:borough_king/widgets/preferiti.dart";
+
 class search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,12 +14,206 @@ class search extends StatelessWidget {
           backgroundColor: Color(0xff90EE90),
           iconTheme: IconThemeData(color: Colors.black)),
 
+      body: ListView(
+        //mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              // Questo container serve per creare la barra di ricerca
+              margin: EdgeInsets.all(8),
+              alignment: Alignment.center,
+              //width: 320,
+              height: 120,
+              //Il bottone è di tipo Raised non so che significa però è bello
+              child: ButtonTheme(   minWidth: 200.0,   height: 70.0,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  onPressed: () {
+                    List<String> borghi = ["Tolfa", "Anagni", "Subiaco", "Bracciano", "Calcata"];
+                    showSearch(context: context, delegate: Search(borghi));
+                    /*Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => notImplemented()),
+            );*/
+                  },
+                  textColor: Colors.black,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          color: Colors.white,
+                          padding: EdgeInsets.fromLTRB(40, 4, 4, 4),
+                          child: Text(
+                            'Inserisci borgo',
+                            style: TextStyle(fontSize: 21, color: Colors.black12),
+                          ),
+                        ),
+                        //Utilizzo i padding per spostare a mio piacimento le icone e il testo
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(90, 0, 0, 0),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.black,
+                            size: 35,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+              child: Text("Consigliati", style: TextStyle(fontSize: 25, color: Colors.black, fontFamily: 'Corbel', fontWeight: FontWeight.bold)),),
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => notImplemented()),
+                );
+              },
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 5,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text('Anagni',style: TextStyle(fontSize: 25),),
+                      subtitle: Text(
+                        'Il borgo del Lazio tra storie e leggende',
+                        style: TextStyle(color: Colors.black.withOpacity(0.8),fontSize: 16, fontWeight: FontWeight.w200, fontFamily: 'Times New Roman'),
+                      ),
+                    ),
+                    Image.asset('assets/images/anagni.jpg'),
+                  ],
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => notImplemented()),
+                );
+              },
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 5,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text('Bracciano',style: TextStyle(fontSize: 25),),
+                      subtitle: Text(
+                        "Il lago, i vicoli pittoreschi e l'atmosfera medievale",
+                        style: TextStyle(color: Colors.black.withOpacity(0.8),fontSize: 16,  fontWeight: FontWeight.w200, fontFamily: 'Times New Roman'),
+                      ),
+                    ),
+                    Image.asset('assets/images/bracciano.jpg'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => notImplemented()),
+                );
+              },
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 5,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text('Calcata',style: TextStyle(fontSize: 25),),
+                      subtitle: Text(
+                        'Il borgo magico degli hippie e delle streghe',
+                        style: TextStyle(color: Colors.black.withOpacity(0.8),fontSize: 16,  fontWeight: FontWeight.w200, fontFamily: 'Times New Roman'),
+                      ),
+                    ),
+                    Image.asset('assets/images/calcata.jpg'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => notImplemented()),
+                );
+              },
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 5,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text('Subiaco',style: TextStyle(fontSize: 25),),
+                      subtitle: Text(
+                        'Il borgo che ospita la "soglia del Paradiso"',
+                        style: TextStyle(color: Colors.black.withOpacity(0.8),fontSize: 16,  fontWeight: FontWeight.w200, fontFamily: 'Times New Roman'),
+                      ),
+                    ),
+                    Image.asset('assets/images/subiaco.jpg'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => notImplemented()),
+                );
+              },
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 5,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text('Tolfa',style: TextStyle(fontSize: 25),),
+                      subtitle: Text(
+                        'Il borgo etrusco ricco di tradizioni',
+                        style: TextStyle(color: Colors.black.withOpacity(0.8),fontSize: 16,  fontWeight: FontWeight.w100, fontFamily: 'Times New Roman'),
+                      ),
+                    ),
+                    Image.asset('assets/images/tolfa.jpg'),
+                  ],
+                ),
+              ),
+            ),
+
+          ]),
+
       bottomNavigationBar: BottomNavigationBar(
         // Serve per inserire la barra inferiore
-        currentIndex: 0,
+        currentIndex: 1,
         type: BottomNavigationBarType.fixed,
         backgroundColor: Color(0xff90EE90),
-        selectedItemColor: Colors.black,
+        selectedItemColor: Colors.black.withOpacity(.60),
         unselectedItemColor: Colors.black.withOpacity(.60),
         selectedFontSize: 15,
         unselectedFontSize: 15,
@@ -30,16 +225,13 @@ class search extends StatelessWidget {
               context,
               MaterialPageRoute(builder: (context) => HomePage()),
             );
-          } else if (value == 1) {
+          } else if (value > 0) {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => search()),
+              MaterialPageRoute(builder: (context) => preferiti()),
             );
-          } else if (value == 2) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => notImplemented()),
-            );
+
+
           }
         },
         items: [
@@ -56,84 +248,6 @@ class search extends StatelessWidget {
             icon: Icon(Icons.panorama),
           ),
         ],
-      ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-      Container(
-      // Questo container serve per creare la barra di ricerca
-      margin: EdgeInsets.all(8),
-      alignment: Alignment.center,
-      //width: 320,
-      height: 120,
-      //Il bottone è di tipo Raised non so che significa però è bello
-      child: ButtonTheme(   minWidth: 200.0,   height: 70.0,
-        child: RaisedButton(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10.0))),
-          onPressed: () {
-            List<String> borghi = ["Tolfa", "Anagni", "Subiaco", "Bracciano", "Calcata"];
-            showSearch(context: context, delegate: Search(borghi));
-            /*Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => notImplemented()),
-            );*/
-          },
-          textColor: Colors.black,
-          color: Colors.white,
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-            child: Row(
-
-              children: <Widget>[
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.fromLTRB(40, 4, 4, 4),
-                  child: Text(
-                    'Inserisci borgo',
-                    style: TextStyle(fontSize: 25, color: Colors.black12),
-                  ),
-                ),
-                //Utilizzo i padding per spostare a mio piacimento le icone e il testo
-                Padding(
-                  padding: EdgeInsets.fromLTRB(70, 0, 0, 0),
-                  child: Icon(
-                    Icons.search,
-                    color: Colors.black,
-                    size: 35,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-      Container(
-            child: Text("Consigliati",style : TextStyle(fontSize : 20)),
-              padding: EdgeInsets.only(right: 250),
-      ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20.0),
-              height: 300.0,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-
-                  Card(
-                    clipBehavior: Clip.antiAlias,
-                    elevation: 5,
-                    child: Column(
-
-                      children: [
-                        Image.asset('assets/images/mercatiniSubiaco.jpg',fit: BoxFit.fill,),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-     ],
       ),
     );
 
@@ -162,17 +276,21 @@ class Search extends SearchDelegate {
     );
   }
   @override
-  Widget buildResults(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("pog"),
-      ),
+  void showResults(BuildContext context) {
+    close(context, query);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => notImplemented()),
     );
   }
+
+  @override
+  Widget buildResults(BuildContext context) => notImplemented();
+
   final List<String> listExample;
   Search(this.listExample);
   List<String> recentList= ['Qui vicino','Tolfa', 'Subiaco'];
-  @override
+
   Widget buildSuggestions(BuildContext context) {
     List<String> suggestionList = [];
     String selectedResult = '';
@@ -185,7 +303,7 @@ class Search extends SearchDelegate {
       itemCount: suggestionList.length,
       itemBuilder: (context, index) {
         return ListTile(
-          leading: Icon(Icons.watch_later_outlined),
+          leading: suggestionList[index] == 'Qui vicino' ?  Icon(Icons.near_me):  Icon(Icons.watch_later_outlined) ,
           title: Text(
             suggestionList[index],
           ),
