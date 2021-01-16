42dd0c7016d747a9e544337358af8f9a0585e5c6
Nakeddo <lorenzo-cristini@hotmail.it>
2021-01-16 21:29:51 +0100

    pog

--- lib/widgets/Borghi_Attivita.dart
+++ lib/widgets/Borghi_Attivita.dart
@@ -9,6 +9,7 @@ import "package:flutter/src/rendering/box.dart";
 import 'package:flutter/src/rendering/sliver_multi_box_adaptor.dart';
 import 'package:borough_king/main.dart';
 import 'package:borough_king/widgets/Tolfa/info.dart';
+import 'package:borough_king/widgets/Tolfa/musei.dart';
 class Attivita {
   String nome;
   String foto;
@@ -762,92 +763,4 @@ class Borghi_Attivita_Calcata extends StatelessWidget {
     );
   }
 }
-class Lista_Attivita extends StatelessWidget {
-  final List<String> List_Attivita = [
-    'Museo Civico',
-    'Museo Civico Archelogico'
-  ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Musei a Tolfa", style: TextStyle(color: Colors.black)),
-        backgroundColor: Color(0xff90EE90),
-        iconTheme: IconThemeData(color: Colors.black),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.black,
-              size: 30,
-            ),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: List_Attivita.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-                onTap: () {},
-                title: Text(List_Attivita[index]),
-                trailing: IconButton(
-                  icon: Icon(
-                    Icons.arrow_forward_ios,
-                    color: Colors.black,
-                    size: 30,
-                  ),
-                  onPressed: () {},
-                )),
-          );
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        // Server per inserire la barra inferiore
-        currentIndex: 2,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Color(0xff90EE90),
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black.withOpacity(.60),
-        selectedFontSize: 13.5,
-        unselectedFontSize: 13.5,
-        onTap: (value) {
-          // Respond to item press.
-          //Ogni bottone ha un indice e gli indici partono da 0 quindi il primo è la home e gli altri a seguire
-          if (value == 0) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          } else if (value == 1) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => preferiti()),
-            );
-          } else if (value == 2) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => occasioniFotografiche()),
-            );
-          }
-        },
-        items: [
-          BottomNavigationBarItem(
-            label: 'Home',
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            label: 'Preferiti',
-            icon: Icon(Icons.favorite),
-          ),
-          BottomNavigationBarItem(
-            label: '   Occasioni\n Fotografiche',
-            icon: Icon(Icons.panorama),
-          ),
-        ],
-      ),
-    );
-  }
-}
---
+++ lib/widgets/Tolfa/musei.dart
@@ -0,0 +1,101 @@
+import 'dart:math';
+import 'package:borough_king/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import "package:flutter/material.dart";
+import "package:borough_king/widgets/preferiti.dart";
+import "package:borough_king/widgets/occasioni_fotografiche.dart";
+import "package:flutter/src/rendering/box.dart";
+import 'package:flutter/src/rendering/sliver_multi_box_adaptor.dart';
+import 'package:borough_king/main.dart';
+
+
+class Lista_Attivita extends StatelessWidget {
+  final List<String> List_Attivita = [
+    'Museo Civico',
+    'Museo Civico Archelogico'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Musei a Tolfa", style: TextStyle(color: Colors.black)),
+        backgroundColor: Color(0xff90EE90),
+        iconTheme: IconThemeData(color: Colors.black),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+              size: 30,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: List_Attivita.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+                onTap: () {},
+                title: Text(List_Attivita[index]),
+                trailing: IconButton(
+                  icon: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  onPressed: () {},
+                )),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // Server per inserire la barra inferiore
+        currentIndex: 2,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color(0xff90EE90),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black.withOpacity(.60),
+        selectedFontSize: 13.5,
+        unselectedFontSize: 13.5,
+        onTap: (value) {
+          // Respond to item press.
+          //Ogni bottone ha un indice e gli indici partono da 0 quindi il primo è la home e gli altri a seguire
+          if (value == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          } else if (value == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => preferiti()),
+            );
+          } else if (value == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => occasioniFotografiche()),
+            );
+          }
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Preferiti',
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            label: '   Occasioni\n Fotografiche',
+            icon: Icon(Icons.panorama),
+          ),
+        ],
+      ),
+    );
+  }
+}
