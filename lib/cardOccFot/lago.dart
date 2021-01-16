3649d1e65aac5a691191519a8ed9ed7029a0387b
unknown <caiweidong123@gmail.com>
2021-01-16 17:46:13 +0100

    attivita borghi

--- lib/cardOccFot/lago.dart
+++ lib/cardOccFot/lago.dart
@@ -8,62 +8,80 @@ class lago extends StatelessWidget {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(235.0),
-        child : AppBar(
-            leading: IconButton(icon: Icon(Icons.clear,size: 35,),onPressed: (){Navigator.pop(context);},),
+        child: AppBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.clear,
+                size: 35,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
             flexibleSpace: Image(
               image: AssetImage('assets/images/OccFotograf/lago.jpg'
                   ''),
               fit: BoxFit.fill,
             ),
             backgroundColor: Colors.transparent,
-
             iconTheme: IconThemeData(color: Colors.white)),
       ),
-      body:
-      ListView(
+      body: ListView(
         children: <Widget>[
           SizedBox(
             height: 25,
           ),
           Row(
             children: [
-              Padding(padding: EdgeInsets.only(right:150
+              Padding(
+                padding: EdgeInsets.only(right: 150),
+                child: Text(
+                  'Lago',
+                  style: TextStyle(fontSize: 30),
+                ),
               ),
-                child: Text('Lago',style: TextStyle(fontSize: 30),
-                ), ),
-              Padding(padding: EdgeInsets.fromLTRB(90, 0, 0, 0),
+              Padding(
+                padding: EdgeInsets.fromLTRB(90, 0, 0, 0),
                 child: IconButton(
                   icon: Icon(Icons.favorite_border),
                   iconSize: 30,
-                  onPressed: () {},),),
+                  onPressed: () {},
+                ),
+              ),
             ],
           ),
           Padding(
             padding: EdgeInsets.only(right: 150),
-            child: Text("Bracciano",style: TextStyle(fontSize: 22, fontFamily: "Times New Roman")),
+            child: Text("Bracciano",
+                style: TextStyle(fontSize: 22, fontFamily: "Times New Roman")),
           ),
-          Text("La veduta dal lago di Bracciano è uno dei panorami più meravigliosi del mondo!" ,style: TextStyle(fontSize: 20),),
+          Text(
+            "La veduta dal lago di Bracciano è uno dei panorami più meravigliosi del mondo!",
+            style: TextStyle(fontSize: 20),
+          ),
           SizedBox(
             height: 10,
           ),
-          Text("Orari:",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-          Text("Tutti i giorni 24 ore su 24",style: TextStyle(fontSize: 18)),
+          Text("Orari:",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          Text("Tutti i giorni 24 ore su 24", style: TextStyle(fontSize: 18)),
           SizedBox(
             height: 10,
           ),
-          Text("Come arrivare?",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-          Text("Partenza: stazione di Bracciano",style: TextStyle(fontSize: 18)),
-          Text("Arrivo: Lago",style: TextStyle(fontSize: 18)),
+          Text("Come arrivare?",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          Text("Partenza: stazione di Bracciano",
+              style: TextStyle(fontSize: 18)),
+          Text("Arrivo: Lago", style: TextStyle(fontSize: 18)),
           SizedBox(
             height: 20,
           ),
           Image(
-            image: AssetImage('assets/images/OccFotograf/comearrivarebracc.JPG'),
+            image:
+                AssetImage('assets/images/OccFotograf/comearrivarebracc.JPG'),
           ),
         ],
       ),
-
     );
-
   }
 }
--- lib/widgets/Borghi_Attivita.dart
+++ lib/widgets/Borghi_Attivita.dart
@@ -17,7 +17,7 @@ class Attivita {
 }
 
 class Borghi_Attivita extends StatelessWidget {
-  List<Attivita> attivita = [
+  final List<Attivita> attivita = [
     Attivita(nome: 'Botteghe', foto: 'store-24px.jpg'),
     Attivita(nome: 'Monumenti e Parchi', foto: 'account_balance-24px.jpg'),
     Attivita(nome: 'Musei', foto: 'museum-24px.jpg'),
@@ -34,11 +34,12 @@ class Borghi_Attivita extends StatelessWidget {
         iconTheme: IconThemeData(color: Colors.black),
         actions: <Widget>[
           IconButton(
-            icon: Icon(
+            icon: IconButton(
+                icon: Icon(
               Icons.search,
               color: Colors.black,
               size: 30,
-            ),
+            )),
             onPressed: () {},
           )
         ],
@@ -51,10 +52,13 @@ class Borghi_Attivita extends StatelessWidget {
             ),
             Container(
                 alignment: Alignment.bottomLeft,
-                child: Icon(
-                  Icons.favorite,
-                  color: Colors.black,
-                  size: 30.0,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.black,
+                    size: 30.0,
+                  ),
+                  onPressed: () {},
                 )),
           ],
         ),
@@ -91,13 +95,108 @@ class Borghi_Attivita extends StatelessWidget {
                     color: Colors.black,
                     size: 30,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Lista_Attivita()));
+                  },
                 ),
               )),
             );
           },
         )
       ]),
+      bottomNavigationBar: BottomNavigationBar(
+        // Server per inserire la barra inferiore
+        currentIndex: 2,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color(0xff90EE90),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black.withOpacity(.60),
+        selectedFontSize: 13.5,
+        unselectedFontSize: 13.5,
+        onTap: (value) {
+          // Respond to item press.
+          //Ogni bottone ha un indice e gli indici partono da 0 quindi il primo è la home e gli altri a seguire
+          if (value == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          } else if (value == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => preferiti()),
+            );
+          } else if (value == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => occasioniFotografiche()),
+            );
+          }
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Preferiti',
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            label: '   Occasioni\n Fotografiche',
+            icon: Icon(Icons.panorama),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Lista_Attivita extends StatelessWidget {
+  final List<String> List_Attivita = [
+    'Museo Civico',
+    'Museo Civico Archelogico'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Musei a Tolfa", style: TextStyle(color: Colors.black)),
+        backgroundColor: Color(0xff90EE90),
+        iconTheme: IconThemeData(color: Colors.black),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+              size: 30,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: List_Attivita.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+                onTap: () {},
+                title: Text(List_Attivita[index]),
+                trailing: IconButton(
+                  icon: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  onPressed: () {},
+                )),
+          );
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         // Server per inserire la barra inferiore
         currentIndex: 2,
